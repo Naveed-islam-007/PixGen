649e22485ab9ae952fb0e62a4c261d97ce44f41c
Naveed-islam-007 <islamnaveed631@gmail.com>
2026-04-28 13:04:51 +0600

    fourth

--- src/app/all-photos/[id]/page.jsx
+++ src/app/all-photos/[id]/page.jsx
@@ -1,13 +1,21 @@
 import { Button, Card, Chip } from "@heroui/react";
+
 import Image from "next/image";
 import Link from "next/link";
 
 
-const PhotoCard = ({photo}) => {
+const PhotoDetails = async({params}) => {
+    const {id}= await params;
+      const res= await fetch('https://pix-gen-beta.vercel.app/data.json')
+    const photos= await res.json()
+    const photo= photos.find(p=> p.id==id);
+    console.log(photo);
     return (
         <div>
+           <h2 className="text-5xl font-bold mb-3">Image Details</h2>
+  
             <Card className="border rounded-xl">
-                <div className="relative w-full aspect-square cursor-pointer hover:scale-105">
+                <div className="relative w-full aspect-square ">
                     <Image src={photo.imageUrl}  alt="image" fill  ></Image>
                     <Chip className="absolute right-2">{photo.category}</Chip>
                 </div>
@@ -18,12 +26,14 @@ const PhotoCard = ({photo}) => {
                     <p>{photo.downloads}</p>
                 </div>
 
-               <Link href={`/all-photos/${photo.id}`} >
-                  <Button variant="outline" className={`w-full`}>View</Button>
-               </Link>
+              <Link href={'/'}>
+                 <Button variant="outline">Back Home</Button>
+              </Link>
             </Card>
+ 
+          
         </div>
     );
 };
 
-export default PhotoCard;
+export default PhotoDetails;
